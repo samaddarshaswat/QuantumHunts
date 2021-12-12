4f58ba7c563b1657d30f657eefe76007518822a4
samaddarshaswat <92857094+samaddarshaswat@users.noreply.github.com>
2021-12-12 13:26:40 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,12 @@
 <?php
 
-//logout.php
+
 
 include('config.php');
 
-//Reset OAuth access token
+
 $google_client->revokeToken();
 
-//Destroy entire session data.
 session_destroy();
 
 //redirect page to index.php
